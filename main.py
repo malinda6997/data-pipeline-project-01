9e336e3a8cd2e655eaed99ac9ec78297bb1e3b2a
malinda6997 <malindaprabath876@gmail.com>
2026-08-31 19:06:21 +0530

    add data quality checks step to ETL pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from etl.extract import extract_products, extract_users
 from etl.transform import transform_products, transform_users
+from etl.quality import check_products_quality, check_users_quality
 from etl.load import load_products, load_users
 
 
@@ -8,21 +9,26 @@ def run_pipeline():
     print("=== Starting Fake Store ETL Pipeline ===")
 
     # Step 1: Extract Data
-    print("\n[1/3] Extracting data from Fake Store API...")
+    print("\n[1/4] Extracting data from Fake Store API...")
     raw_products_df = extract_products()
     raw_users_df = extract_users()
 
     if raw_products_df.empty or raw_users_df.empty:
         print("Pipeline aborted: Failed to extract data.")
         sys.exit(1)
- 
+
     # Step 2: Transform Data
-    print("\n[2/3] Transforming extracted data...")
+    print("\n[2/4] Transforming extracted data...")
     clean_products_df = transform_products(raw_products_df)
     clean_users_df = transform_users(raw_users_df)
 
-    # Step 3: Load Data into AWS RDS PostgreSQL
-    print("\n[3/3] Loading clean data into PostgreSQL database...")
+    # Step 3: Data Quality Checks
+    print("\n[3/4] Performing Data Quality Checks...")
+    check_products_quality(clean_products_df)
+    check_users_quality(clean_users_df)
+
+    # Step 4: Load Data into AWS RDS PostgreSQL
+    print("\n[4/4] Loading clean data into PostgreSQL database...")
     load_products(clean_products_df)
     load_users(clean_users_df)
 
